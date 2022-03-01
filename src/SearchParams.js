91f0ee8634fe13d510b619c2d8f9998ca3d7f0f5
Alexandre Gressier <alexandre@gressier.dev>
2022-03-01 21:59:14 +0100

    Use hooks to define selects for both animal type & breed

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,62 @@
+import {useState} from "react"
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"]
+
 const SearchParams = () => {
-  const location = "Seattle, WA"
+  const [location, setLocation] = useState("Seattle, WA")
+  const [animal, setAnimal] = useState("")
+  const [breed, setBreed] = useState("")
+  const breeds = []
 
   return <div className="search-params">
     <form>
       <label htmlFor="location">
         Location
-        <input type="text" id="location" value={location} placeholder="Location"/>
+        <input
+          id="location"
+          type="text"
+          placeholder="Location"
+          value={location}
+          onChange={e => setLocation(e.target.value)}
+        />
+      </label>
+      <label htmlFor="animal">
+        Animal
+        <select
+          id="animal"
+          value={animal}
+          onChange={e => {
+            setAnimal(e.target.value)
+            setBreed("")
+          }}
+          onBlur={e => {
+            setAnimal(e.target.value)
+            setBreed("")
+          }}
+        >
+          <option/>
+          {ANIMALS.map(animal =>
+            <option key={animal} value={animal}>
+              {animal}
+            </option>
+          )}
+        </select>
+      </label>
+      <label htmlFor="breed">
+        Breed
+        <select
+          id="breed"
+          value={breed}
+          onChange={e => setBreed(e.target.value)}
+          onBlur={e => setBreed(e.target.value)}
+        >
+          <option/>
+          {breeds.map(breed =>
+            <option key={breed} value={breed}>
+              {breed}
+            </option>
+          )}
+        </select>
       </label>
     </form>
   </div>
