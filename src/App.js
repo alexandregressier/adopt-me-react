62a7be3ca1a164d0f8a1a061d7095cfd4e70bce9
Alexandre Gressier <alexandre@gressier.dev>
2022-03-01 21:03:14 +0100

    Migrate to JSX

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import React from "react"
 import {render} from "react-dom"
 import Pet from "./Pet"
 
 const App = () =>
-  React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Daïla",
-        animal: "Dog",
-        breed: "Golden Retriever",
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel",
-      }),
-      React.createElement(Pet, {
-        name: "Doink",
-        animal: "Cat",
-        breed: "Mix",
-      }),
-    ],
-  )
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Daïla" animal="dog" breed="Golden Retriever"/>
+    <Pet name="Pepper" animal="bird" breed="Cockatiel"/>
+    <Pet name="Doink" animal="cat" breed="Mix"/>
+  </div>
 
-render(React.createElement(App), document.getElementById("root"))
+render(<App/>, document.getElementById("root"))
--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,8 @@
-import React from "react"
-
 const Pet = (props) =>
-  React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ])
+  <div>
+    <h1>{props.name}</h1>
+    <h2>{props.animal}</h2>
+    <h2>{props.breed}</h2>
+  </div>
 
 export default Pet
