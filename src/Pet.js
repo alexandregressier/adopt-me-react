53aff171371a16d08823b53dde886a143b466a81
Alexandre Gressier <alexandre@gressier.dev>
2022-03-02 17:19:48 +0100

    Improve the Pet component

--- src/Pet.js
+++ src/Pet.js
@@ -1,8 +1,19 @@
-const Pet = props =>
-  <div>
-    <h1>{props.name}</h1>
-    <h2>{props.animal}</h2>
-    <h2>{props.breed}</h2>
+const Pet = ({name, animal, breed, images, location, id}) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg"
+  if (images.length > 0) {
+    hero = images[0]
+  }
+  return <div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{animal} – {breed} – {location}</h2>
+      </div>
+    </a>
   </div>
+}
 
 export default Pet
