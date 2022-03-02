66f63ad9b687030814d4441df824a3e9cfc77a79
Alexandre Gressier <alexandre@gressier.dev>
2022-03-02 20:02:23 +0100

    Rewrite the Details component as a class component

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,37 @@
+import {Component} from "react"
 import {useParams} from "react-router-dom"
 
-const Details = () => {
-  const {id} = useParams()
-  return <div>
-    <h1>Details for {id}</h1>
-  </div>
+class Details extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {loading: true}
+  }
+
+  async componentDidMount() {
+    const res = await fetch(`http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`)
+    const json = await res.json()
+    this.setState({loading: false, ...json.pets[0]})
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>Loading...</h2>
+    }
+    const {animal, breed, city, state, description, name} = this.state
+    return <div className="details">
+      <div>
+        <h1>{name}</h1>
+        <h2>{animal} – {breed} – {city}, {state}</h2>
+        <button>Adopt {name}</button>
+        <p>{description}</p>
+      </div>
+    </div>
+  }
 }
 
-export default Details
+const WrappedDetails = () => {
+  const params = useParams()
+  return <Details params={params}/>
+}
+
+export default WrappedDetails
